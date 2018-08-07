490db511d63a1edb14f14a09bc272227897cc1f3
Jeremy M. Gottwig <jgottwig@umd.edu>
2018-08-07 12:57:40 -0400

    LIBWEB-4314. Modifying port.
    https://issues.umd.edu/browse/LIBWEB-4314

--- scripts/load-csv.sh
+++ scripts/load-csv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
 
-curl "http://localhost:8993/solr/irroc/update/csv?commit=true&f.meta_description.split=true&f.meta_description.separator=,&f.stage_list.split=true&f.stage_list.separator=;&f.task_list.split=true&f.task_list.separator=;&f.stage_list_facet.split=true&f.stage_list_facet.separator=;&f.task_list_facet.split=true&f.task_list_facet.separator=;" --data-binary @$1 -H 'Content-type:text/csv; charset=utf-8'
+curl "http://localhost:8983/solr/irroc/update/csv?commit=true&f.meta_description.split=true&f.meta_description.separator=,&f.stage_list.split=true&f.stage_list.separator=;&f.task_list.split=true&f.task_list.separator=;&f.stage_list_facet.split=true&f.stage_list_facet.separator=;&f.task_list_facet.split=true&f.task_list_facet.separator=;" --data-binary @$1 -H 'Content-type:text/csv; charset=utf-8'
 
